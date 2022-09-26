167fac9d4a6097e01fec0446ad59540b802ad28f
Christina <you@example.comchristinanicosia08@gmail.comgit config --global user.name Your>
2022-09-26 16:03:31 -0700

    created function to add restaurants to dictionary

--- ratings.py
+++ ratings.py
@@ -2,3 +2,17 @@
 
 
 # put your code here
+
+def restaurant_ratings(input_file):
+
+    restaurants = {}
+    # words = line.rstrip().split()
+    for line in input_file:
+        restaurant_rating = line.rstrip().split(":")
+        restaurant, rating = restaurant_rating
+        restaurants[restaurant] = rating
+        print(f"{restaurant} is rated at {rating}.") 
+
+
+with open("scores.txt") as input_file:
+    restaurant_ratings(input_file)
