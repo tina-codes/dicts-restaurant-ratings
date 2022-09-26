4a52e2e1be4113e57b2f1b1210228a39f7028c9e
Christina <you@example.comchristinanicosia08@gmail.comgit config --global user.name Your>
2022-09-26 16:12:13 -0700

    prints sorted list of ratings

--- ratings.py
+++ ratings.py
@@ -5,13 +5,18 @@
 
 def restaurant_ratings(input_file):
 
+    # sorted(animals.items())
     restaurants = {}
     # words = line.rstrip().split()
-    for line in input_file:
+    for line in sorted(input_file):
+        
         restaurant_rating = line.rstrip().split(":")
         restaurant, rating = restaurant_rating
         restaurants[restaurant] = rating
         print(f"{restaurant} is rated at {rating}.") 
+
+    #for restaurant in sorted(restaurants.items()):
+        #print(f"{restaurant} is rated at {restaurants[restaurant]}.")   
 
 
 with open("scores.txt") as input_file:
